2d4d5648f2d9e0e39538248456da73f5f9834e55
Aleksander Kozłowski <olek4004@gmail.com>
2023-03-29 20:30:20 +0200

    Add files via upload

---
+++ OpPsZadanie3/src/Main.java
@@ -0,0 +1,104 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static class Point {
+        private int x;
+        private int y;
+
+        public Point() {
+            this.x = 0;
+            this.y = 0;
+        }
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        // konstruktor kopiujący, chuj wie o co chodzi
+    }
+
+    public static class Section {
+        private int pointA;
+        private int pointB;
+
+        public Section() {
+            this.pointA = 0;
+            this.pointB = 0;
+        }
+
+        public Section(int pointA, int pointB) {
+            this.pointA = pointA;
+            this.pointB = pointB;
+        }
+
+        // głęboki konstruktor kopiujący, głębokie to ty masz gardło debilu
+    }
+
+    public static class Circle {
+
+        private int srodek;
+        private double promien;
+
+        public Circle() {
+            this.srodek = 0;
+            this.promien = 0;
+        }
+
+        public Circle(int srodek, double promien) {
+            this.srodek = srodek;
+            this.promien = promien;
+        }
+
+        // głęboki konstruktor
+    }
+
+    public static class Picture {
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String opcja;
+
+        do {
+            opcja = "";
+            System.out.println(
+                    "1 Dodaj do obrazu\n2 Wyświetl Obraz\n3 Przesuń Obraz\n4 Wyświetl Sumę Pól\nw Wyjdź\nWybierz>");
+            try {
+                opcja = scanner.nextLine();
+            } catch (InputMismatchException e) {
+                System.out.println("Brak Opcji");
+            }
+
+            switch (opcja) {
+                case "1":
+                    opcja = "";
+                    System.out.println("1 Punkt\n2 Odcinek\n3 Okrąg\nWybierz>");
+                    try {
+                        opcja = scanner.nextLine();
+                    } catch (InputMismatchException e) {
+                        System.out.println("Brak Opcji");
+                    }
+                    switch (opcja) {
+                        case "1":
+                            break;
+                        case "2":
+                            break;
+                        case "3":
+                            break;
+                    }
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+            }
+        } while (!"w".equals(opcja));
+
+        scanner.close();
+    }
+}
